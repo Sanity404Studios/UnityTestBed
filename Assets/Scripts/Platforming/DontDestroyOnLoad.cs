b34a8cf6af8528d4894b046b75d48b382e2cd58f
TheDeathsinger <tarnthelos@gmail.com>
2016-01-27 15:54:05 -0500

    New level,  door to transport between levels
    Does what it says on the tin

---
+++ Assets/Scripts/Platforming/DontDestroyOnLoad.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DontDestroyOnLoad : MonoBehaviour {
+
+    static DontDestroyOnLoad Instance;
+    void Start()
+    {
+        if(Instance != null)
+        {
+            GameObject.Destroy(gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(gameObject);
+            Instance = this;
+        }
+
+        Debug.LogWarning(Instance);
+    }
+}
---
+++ Assets/Scripts/Platforming/SceneManager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SceneManager : MonoBehaviour {
+
+    int loadedLevel;
+
+    void Start()
+    {
+        loadedLevel = Application.loadedLevel;
+        Debug.LogWarning("Loaded Level: " + Application.loadedLevel + " variable for loaded level: " + loadedLevel);
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if ("Player" == other.gameObject.tag)
+        {
+            Debug.LogWarning("If has been entered, loaded level value " + loadedLevel);
+            loadedLevel++;
+            Debug.LogWarning("Value after addition" + loadedLevel);
+            Application.LoadLevel(loadedLevel);
+        }
+    }
+}
