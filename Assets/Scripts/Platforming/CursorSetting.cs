c47d8c969358747d8d76a584cb9ab07529d158e6
TheDeathsinger <tarnthelos@gmail.com>
2016-01-31 14:08:29 -0500

    First attempts at a cursor

---
+++ Assets/Scripts/Platforming/CursorSetting.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CursorSetting : MonoBehaviour {
+
+    public Texture2D newCursorTexture;
+    static CursorSetting Instance;
+
+    void Awake()
+    {
+        if(null != Instance)
+        {
+            GameObject.Destroy(gameObject);
+        }
+        else
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    void Start()
+    {
+        Cursor.SetCursor(newCursorTexture, Vector2.zero, CursorMode.Auto);
+    }
+}
