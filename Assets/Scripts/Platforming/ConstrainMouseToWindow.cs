59d6dfef6e3c330f5d3d127716b6f8a9d272bcbc
TheDeathsinger <tarnthelos@gmail.com>
2016-02-29 11:45:45 -0500

    Attempt at contraining mouse to window
    I've since found out that you can't contraint the mouse just inside
    Unity, and you have to import other DLLs in order to do that. But that
    import feature is pro only.

---
+++ Assets/Scripts/Platforming/ConstrainMouseToWindow.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Runtime.InteropServices;
+
+public class ConstrainMouseToWindow : MonoBehaviour {
+
+    //Creates new screen rect who's boundries ar the width and height of the screen
+    Rect screenRect = new Rect(0, 0, Screen.width, Screen.height);
+	
+	// Update is called once per frame
+	void Update () {
+        Vector2 mousePos = Input.mousePosition;
+
+        if(false == screenRect.Contains(mousePos))
+        {
+            Debug.Log("Mouse out of bounds! " + mousePos);
+            return;
+        }
+	}
+}
