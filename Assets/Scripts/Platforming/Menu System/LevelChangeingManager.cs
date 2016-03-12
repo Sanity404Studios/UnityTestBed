df5cb5ac4b8878f8019d6de23018c7410a25fa7e
TheDeathsinger <tarnthelos@gmail.com>
2016-03-11 19:38:48 -0500

    Switching over systems for level management

---
+++ Assets/Scripts/Platforming/Menu System/LevelChangeingManager.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class LevelChangeingManager : MonoBehaviour {
+
+    static void SwitchLevelDeletion(int sceneBuildIndexToLoad)
+    {
+        SceneManager.LoadScene(sceneBuildIndexToLoad);
+    }
+
+    static void OnPlayerDeath()
+    {
+
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if("Player" == other.collider.name)
+        {
+            LoadNextLevel();
+        }
+    }
+
+    static void LoadNextLevel()
+    {
+        int currLevel = SceneManager.GetActiveScene().buildIndex;
+
+        if(currLevel >= SceneManager.sceneCountInBuildSettings)
+        {
+            //Return to main menu with text saying you win
+        }
+        
+        SwitchLevelDeletion(currLevel++);
+    }
+}
